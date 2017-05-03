f2717069c661bc0f4a0fcb739511231e5dffff68
Sjors Provoost <sjors@sprovoost.nl>
2017-05-03 12:30:13 +0200

    refactor(Address): convert to ES6

--- src/address.js
+++ src/address.js
@@ -1,44 +1,33 @@
-'use strict';
+class Address {
+  constructor (obj) {
+    this._street = obj.street;
+    this._city = obj.city;
+    this._state = obj.state;
+    this._zipcode = obj.zipcode;
+    this._country = obj.country;
+  }
 
-module.exports = Address;
 
-function Address (obj) {
-  this._street = obj.street;
-  this._city = obj.city;
-  this._state = obj.state;
-  this._zipcode = obj.zipcode;
-  this._country = obj.country;
+  get city () {
+    return this._city;
+  }
+
+  get country () {
+    return this._country;
+  }
+
+  // ISO 3166-2, the part after the dash
+  get state () {
+    return this._state;
+  }
+
+  get street () {
+    return this._street;
+  }
+
+  get zipcode () {
+    return this._zipcode;
+  }
 }
 
-Object.defineProperties(Address.prototype, {
-  'city': {
-    configurable: false,
-    get: function () {
-      return this._city;
-    }
-  },
-  'country': {
-    configurable: false,
-    get: function () {
-      return this._country;
-    }
-  },
-  'state': { // ISO 3166-2, the part after the dash
-    configurable: false,
-    get: function () {
-      return this._state;
-    }
-  },
-  'street': {
-    configurable: false,
-    get: function () {
-      return this._street;
-    }
-  },
-  'zipcode': {
-    configurable: false,
-    get: function () {
-      return this._zipcode;
-    }
-  }
-});
+module.exports = Address;
