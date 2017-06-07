b9ee6a5003e249cc2e92d37d7135d3d4524d50de
Sjors Provoost <sjors@sprovoost.nl>
2017-06-07 13:10:22 +0200

    refactor(BankAccount): convert to class

--- src/bank-account.js
+++ src/bank-account.js
@@ -1,51 +1,36 @@
-'use strict';
+class BankAccount {
+  constructor (obj) {
+    this._type = obj.bank_account_type;
+    this._currency = 'INR';
+    this._ifsc = obj.bank_ifsc_code;
+    this._number = obj.bank_account_number;
+    this._bank_name = obj.bank_name;
+    this._holder_name = obj.bank_account_name;
+  }
 
-module.exports = BankAccount;
+  get type () {
+    return this._type;
+  }
 
-function BankAccount (obj) {
-  this._type = obj.bank_account_type;
-  this._currency = 'INR';
-  this._ifsc = obj.bank_ifsc_code;
-  this._number = obj.bank_account_number;
-  this._bank_name = obj.bank_name;
-  this._holder_name = obj.bank_account_name;
+  get currency () {
+    return this._currency;
+  }
+
+  get ifsc () {
+    return this._ifsc;
+  }
+
+  get number () {
+    return this._number;
+  }
+
+  get bankName () {
+    return this._bank_name;
+  }
+
+  get holderName () {
+    return this._holder_name;
+  }
 }
 
-Object.defineProperties(BankAccount.prototype, {
-  'type': {
-    configurable: false,
-    get: function () {
-      return this._type;
-    }
-  },
-  'currency': {
-    configurable: false,
-    get: function () {
-      return this._currency;
-    }
-  },
-  'ifsc': {
-    configurable: false,
-    get: function () {
-      return this._ifsc;
-    }
-  },
-  'number': {
-    configurable: false,
-    get: function () {
-      return this._number;
-    }
-  },
-  'bankName': {
-    configurable: false,
-    get: function () {
-      return this._bank_name;
-    }
-  },
-  'holderName': {
-    configurable: false,
-    get: function () {
-      return this._holder_name;
-    }
-  }
-});
+module.exports = BankAccount;
