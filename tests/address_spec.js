7f9525eba795164bb17f8a3c3f37822905056bfb
Sjors Provoost <sjors@sprovoost.nl>
2017-06-14 16:05:31 +0200

    test(Address): 100% coverage
    Tests for setters are rather trivial, but they may get more interesting
    if validation is added.

--- tests/address_spec.js
+++ tests/address_spec.js
@@ -33,15 +33,100 @@ describe('Address', function () {
     )
   );
 
-  describe('instance', () =>
-    describe('getters', () =>
+  describe('instance', () => {
+    describe('getters', () => {
       it('should work', function () {
         expect(a.street).toBe(aObj.street);
         expect(a.city).toBe(aObj.city);
         expect(a.state).toBe(aObj.state);
         expect(a.zipcode).toBe(aObj.zipcode);
         expect(a.country).toBe(aObj.country);
-      })
-    )
-  );
+      });
+
+      describe('complete', () => {
+        it('should be false if e.g. city is not set', () => {
+          a._city = undefined;
+          expect(a.complete).toEqual(false);
+        });
+
+        it('should be true if all fields are set', () => {
+          expect(a.complete).toEqual(true);
+        });
+      });
+    });
+
+    describe('setters', () => {
+      describe('city', () => {
+        it('should set dirty', () => {
+          a.city = 'Delhi';
+          expect(a.dirty).toEqual(true);
+        });
+
+        it('should not set dirty if value didnt change', () => {
+          a.city = 'Bangalore';
+          expect(a.dirty).toEqual(false);
+        });
+      });
+
+      describe('country', () => {
+        it('should set dirty', () => {
+          a.country = 'AU';
+          expect(a.dirty).toEqual(true);
+        });
+
+        it('should not set dirty if value didnt change', () => {
+          a.country = 'IN';
+          expect(a.dirty).toEqual(false);
+        });
+      });
+
+      describe('state', () => {
+        it('should set dirty', () => {
+          a.state = 'Kerala';
+          expect(a.dirty).toEqual(true);
+        });
+
+        it('should not set dirty if value didnt change', () => {
+          a.state = 'Karnataka';
+          expect(a.dirty).toEqual(false);
+        });
+      });
+
+      describe('street', () => {
+        it('should set dirty', () => {
+          a.street = 'Abc #1024 7th cross';
+          expect(a.dirty).toEqual(true);
+        });
+
+        it('should not set dirty if value didnt change', () => {
+          a.street = 'Abc #1024 6th cross';
+          expect(a.dirty).toEqual(false);
+        });
+      });
+
+      describe('zipcode', () => {
+        it('should set dirty', () => {
+          a.zipcode = '560012';
+          expect(a.dirty).toEqual(true);
+        });
+
+        it('should not set dirty if value didnt change', () => {
+          a.zipcode = '560011';
+          expect(a.dirty).toEqual(false);
+        });
+      });
+    });
+
+    describe('didSave', () => {
+      // This method is called by profile.js after it's synced with the API
+      beforeEach(() => {
+        a.city = 'Delhi'; // this sets a._dirty to true
+      });
+
+      it('should unset dirty', () => {
+        a.didSave();
+        expect(a.dirty).toEqual(false);
+      });
+    });
+  });
 });
