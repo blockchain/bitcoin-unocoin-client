7efd2c3f72d83af6186531622db77d790993d563
Sjors Provoost <sjors@sprovoost.nl>
2017-06-26 18:22:08 +0100

    BREAKING(Profile): renamed completed partial methods, more tests
    Renamed addressComplete to identityComplete and infoComplete to
    bankInfoComplete.

--- tests/profile_spec.js
+++ tests/profile_spec.js
@@ -20,7 +20,21 @@ describe('Profile', function () {
         status_code: 200,
         message: 'Unverified User',
         user_status: 1
-      })
+      }),
+      authPOST: (method, payload) => {
+        if (payload.name !== 'Should Fail') {
+          return Promise.resolve({
+            status_code: 200,
+            user_status: 2
+          });
+        } else {
+          return Promise.resolve({
+            status_code: 714,
+            user_status: 2,
+            message: 'FAIL'
+          });
+        }
+      }
     };
 
     newUserObj = {
@@ -110,11 +124,193 @@ describe('Profile', function () {
     });
   });
 
-  describe('instance', function () {
-    // let newUserProfile;
+  describe('level 1', () => {
+    let profile;
 
-    beforeEach(function () {
-      // let newUserProfile = new Profile(newUserObj);
+    beforeEach(() => {
+      profile = new Profile(newUserObj, api);
+    });
+
+    it('should not be read-only', () => {
+      expect(profile.readOnly).toBeFalsy();
+    });
+
+    it('should not be dirty by default', () => {
+      expect(profile.dirty).toBeFalsy();
+    });
+
+    it('should be dirty if a field is changed', () => {
+      profile.fullName = 'John Do';
+      expect(profile.dirty).toBeTruthy();
+    });
+
+    it('should not be dirty if a field is not changed', () => {
+      profile.fullName = 'John Do';
+      profile.mobile = '1234567893';
+      profile.pancard = 'BAD876G570';
+      profile.bankAccountNumber = '123';
+      profile.ifsc = '456';
+      profile._dirty = false; // Pretend we saved
+
+      profile.fullName = 'John Do';
+      profile.mobile = '1234567893';
+      profile.pancard = 'BAD876G570';
+      profile.bankAccountNumber = '123';
+      profile.ifsc = '456';
+      expect(profile.dirty).toBeFalsy();
+    });
+
+    describe('photos complete', () => {
+      it('should check address, pancard and photo present', () => {
+        expect(profile.photosComplete).toBeFalsy();
+        profile._photos = {
+          address: 'base64',
+          pancard: 'base64',
+          photo: 'base64'
+        };
+        expect(profile.photosComplete).toBeTruthy();
+      });
+    });
+
+    describe('address complete', () => {
+      it('should check mobile, pancard full name and address details present', () => {
+        expect(profile.identityComplete).toBeFalsy();
+        profile.mobile = '123';
+        profile.pancard = '456';
+        profile.fullName = 'John Do';
+        profile._address = {complete: true};
+        expect(profile.identityComplete).toBeTruthy();
+      });
+    });
+
+    describe('info complete', () => {
+      it('should check IFSC and bank account', () => {
+        expect(profile.bankInfoComplete).toBeFalsy();
+        profile.ifsc = '123';
+        profile.bankAccountNumber = '456';
+        expect(profile.bankInfoComplete).toBeTruthy();
+      });
+    });
+
+    describe('complete', () => {
+      it('should check all the things', () => {
+        expect(profile.complete).toBeFalsy();
+
+        spyOnProperty(profile, 'photosComplete').and.returnValue(true);
+        spyOnProperty(profile, 'identityComplete').and.returnValue(true);
+        spyOnProperty(profile, 'bankInfoComplete').and.returnValue(true);
+
+        expect(profile.complete).toBeTruthy();
+      });
+    });
+
+    describe('verify()', () => {
+      beforeEach(() => {
+        profile.fullName = 'John Do';
+        profile.mobile = '+91234567893';
+        profile.pancard = 'BAD876G570';
+        profile.bankAccountNumber = '123';
+        profile.ifsc = '456';
+        profile._photos = {
+          address: {base64: 'base64'},
+          pancard: {base64: 'base64'},
+          photo: {base64: 'base64'}
+        };
+        profile._address = {
+          street: '1',
+          state: 'K',
+          city: 'Bangalore',
+          zipcode: '123',
+          country: 'IN',
+          complete: true,
+          didSave: () => {}
+        };
+
+        spyOn(api, 'authPOST').and.callThrough();
+      });
+
+      it('calls settings/uploaduserprofile', done => {
+        let checks = () => {
+          expect(api.authPOST).toHaveBeenCalled();
+          expect(api.authPOST.calls.argsFor(0)[0]).toEqual(('api/v1/settings/uploaduserprofile'));
+        };
+
+        profile.verify().then(checks).catch(fail).then(done);
+      });
+
+      it('should set readOnly to true', (done) => {
+        let checks = () => {
+          expect(profile.readOnly).toBeTruthy();
+        };
+
+        profile.verify().then(checks).catch(fail).then(done);
+      });
+
+      it('should set dirty to false', (done) => {
+        let checks = () => {
+          expect(profile.dirty).toBeFalsy();
+        };
+
+        profile.verify().then(checks).catch(fail).then(done);
+      });
+
+      describe('fails', () => {
+        beforeEach(() => {
+          profile.fullName = 'Should Fail';
+        });
+
+        it('should return the error message', (done) => {
+          let checks = (message) => {
+            expect(message).toEqual('FAIL');
+          };
+
+          profile.verify().then(fail).catch(checks).then(done);
+        });
+
+        it('should leave readOnly false', (done) => {
+          let checks = () => {
+            expect(profile.readOnly).toBeFalsy();
+          };
+
+          profile.verify().then(fail).catch(checks).then(done);
+        });
+
+        it('should remain dirty', (done) => {
+          let checks = () => {
+            expect(profile.dirty).toBeTruthy();
+          };
+
+          profile.verify().then(fail).catch(checks).then(done);
+        });
+      });
+    });
+  });
+
+  describe('level 2', () => {
+    let profile;
+
+    beforeEach(() => {
+      profile = new Profile(pendingVerificationUserObj, api);
+    });
+
+    it('should be read-only', () => {
+      expect(profile.readOnly).toBeTruthy();
+    });
+  });
+
+  describe('level 3', () => {
+    let profile;
+
+    beforeEach(() => {
+      profile = new Profile(verifiedUserObj, api);
+    });
+
+    it('should be read-only', () => {
+      expect(profile.readOnly).toBeTruthy();
+    });
+
+    it('sets the current limits', () => {
+      expect(profile.currentLimits.bank.inRemaining).toEqual(1000);
     });
   });
 });
